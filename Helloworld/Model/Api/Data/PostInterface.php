c889e9e8ed523afea79b128d31fc4a6ac4a87cfd
malindersingh <malindersingh94@gmail.com>
2017-03-29 17:22:25 +0530

    Add files via upload

---
+++ Helloworld/Model/Api/Data/PostInterface.php
@@ -0,0 +1,12 @@
+<?php
+namespace Inchoo\Helloworld\Model\Api\Data;
+interface PostInterface {
+	public function getId();
+	public function setId();
+
+	public function getName();
+	public function setName();
+
+	public function getPostContent();
+	public function setPostContent();
+}
